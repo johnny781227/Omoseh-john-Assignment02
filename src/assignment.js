03c779c5c604a25f2285671335b541841cc46326
john omoseh <104631668+johnny781227@users.noreply.github.com>
2022-07-17 05:38:37 +0800

    Update assignment.js

--- src/assignment.js
+++ src/assignment.js
@@ -4,7 +4,7 @@ const assignment = {};
 // ========================
 
 
-/**
+
  * Challenge - 1
  * 
  * Calculate and return the sum of the numbers in an array.
@@ -14,11 +14,15 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let sum=0
+    for(let i=o; i < arrayOfNumbers.length; i++){
+    sum+= arrayOfNumbers[i]
+    }
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+ assignment.sumOfNumbers = sumOfNumbers;
 
-/**
+
  * Challenge - 2
  * 
  * Count all even numbers within an array of numbers.
@@ -28,7 +32,12 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    for(let=0; i < arrayOfNumbers.length; i++){
+        if(arrayOfNumbers[i] % 2 == 0) {
+        number.push(arrayOfNumbers[i]);
+        };
+    };
+    return number.length;
 }
 // assignment.countEvenNumbers = countEvenNumbers;
 
